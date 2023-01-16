5d4ac693ac7465b6aad4af9744e5aaff9ae80990
Aroop Biswal <aroopbiswal@gmail.com>
2023-01-15 16:00:41 -0800

    minor changes

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
+import "./.env";
+
 
 
 //components
@@ -11,6 +15,17 @@ import SearchPage from './pages/searchPage/searchPage';
 
 export default function App() {
 
+  const[bearerSet, setBearerSet] = useState(false);
+
+  useEffect(() => {
+    const bearer = localStorage.getItem("bearer");
+    if (!bearerSet) {
+      console.log("loaded");
+      console.log(process.env.BEARER);
+      setBearerSet(true);
+    }
+    
+ });
   return (
     <Router>
       <NavBar />
